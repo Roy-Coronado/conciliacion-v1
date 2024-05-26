a3646ebff4950159a42eb10d3240d649e271b0c9
Roy-Coronado <royelias-227-@hotmail.com>
2024-05-25 21:06:26 -0500

    Nuevos Agregados

---
+++ html/reportes.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reportes.css" >
+    <title>Asistencia</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+        <div class="principal">
+            <h2 class="title">Procesar Datos</h2>
+            <div class="contenido-principal">
+                <div class="lado-izquierdo">
+                    <label> Seleccionar Archivo </label>
+                    <input type="file" name="" id="">
+                </div>
+                <div class="boton">
+                    <button type="button"> Procesar Datos </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="reportes">
+            <div>
+                <label for=""> Desde </label>
+                <input type="date" name="" id="">
+            </div>
+            <div>
+                <label for=""> Hasta </label>
+                <input type="date" name="" id="">
+            </div>
+            <div>
+                <label for=""> Nombre </label>
+                <input type="text" name="" id="">
+            </div>
+            <div>
+                <button type="button">Buscar</button>
+            </div>
+        </div>
+    </form>
+
+</body>
+
+</html>
